b7b23927f5f35cfecede53459f6448ca4d744877
Marc-Étienne <>
2019-07-28 20:01:56 -0400

    Update side nav menu

--- angular/front-end/src/app/app.angular.material.module.ts
+++ angular/front-end/src/app/app.angular.material.module.ts
@@ -1,9 +1,19 @@
 import {NgModule} from '@angular/core';
+
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatListModule} from  '@angular/material/list';
+import {MatIconModule} from  '@angular/material/icon';
 
 @NgModule({
   exports: [
-    MatSidenavModule
+    MatSidenavModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatListModule,
+    MatIconModule,
+    
   ]
 })
 export class AngularMaterialModule {}
