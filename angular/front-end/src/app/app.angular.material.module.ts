e91429f90a2717eb5c9bffe075d299348663dc67
bmw546 <marc-etienne-pepin@hotmail.com>
2019-07-29 21:38:25 -0400

    start a little form on menu

--- angular/front-end/src/app/app.angular.material.module.ts
+++ angular/front-end/src/app/app.angular.material.module.ts
@@ -1,11 +1,13 @@
-import {NgModule} from '@angular/core';
+import { NgModule } from '@angular/core';
 
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonModule} from '@angular/material/button';
-import {MatListModule} from  '@angular/material/list';
-import {MatIconModule} from  '@angular/material/icon';
-
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   exports: [
     MatSidenavModule,
@@ -13,7 +15,9 @@ import {MatIconModule} from  '@angular/material/icon';
     MatButtonModule,
     MatListModule,
     MatIconModule,
-    
+    MatInputModule,
+    FormsModule,
+    BrowserAnimationsModule,
   ]
 })
-export class AngularMaterialModule {}
+export class AngularMaterialModule { }
