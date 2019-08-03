5f2b13b0d645dff32fcdc0d49733779ae751e87b
bmw546 <marc-etienne-pepin@hotmail.com>
2019-08-02 23:29:44 -0400

    WIP

--- angular/front-end/src/app/media/images-gallery/images-gallery.component.ts
+++ angular/front-end/src/app/media/images-gallery/images-gallery.component.ts
@@ -7,9 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImagesGalleryComponent implements OnInit {
 
-  constructor() { }
+  public LightBoxElement;
+  public Images;      // Array of images that will be used to show.
+  constructor() { 
+    this.LightBoxElement = undefined;
+  }
 
   ngOnInit() {
   }
 
+
+  onclick(element){
+    this.LightBoxElement = element;
+  }
+
+  existLightBox(){
+
+    if(this.LightBoxElement !== undefined){
+      let modal = document.getElementById(this.LightBoxElement)
+    }
+    this.LightBoxElement = undefined;
+  }
+
+
+  
 }
+/*
+// -------------------------- LIGHT BOX ------------------------------------------
+// pour les lightbox( aka "shadowbox")
+function lightbox(who){
+    document.getElementById(who).style.display = "block";
+    document.getElementById('lightBoxBg').style.display = "block";
+
+}
+// tour de passe passe pour que l'image soit au dessus. (si on le fait direct javascript vérifier les class name avant
+// que le reste du code php soit executer for some reason)
+function stop(){
+    stop2();
+}
+function stop2(){
+    var elements = document.getElementsByClassName('lightBox');
+    for(var i=0; i<elements.length; i++) {
+        elements[i].style.display='none';
+    }
+    document.getElementById('lightBoxBg').style.display = "none";
+}
+*/
