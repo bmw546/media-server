f0df0a98ba3bef00cc96296b528fdd6518c6fdac
bmw546 <marc-etienne-pepin@hotmail.com>
2020-07-05 11:56:06 -0400

    wip

---
+++ back-end/node_modules/module/image/dao/image-dao.js
@@ -0,0 +1,66 @@
+const IBaseDao = require('servercore/dao/i-base-dao');
+
+const ImageEntity = require('../entities/image-entity');
+
+const {postGres} = require('servercore/postgres/postgresPipe');
+
+
+// --------------- Let add the basic table --------------------
+// Maybe ask for a better generated ID
+postGres.addCreateTable(
+    `CREATE TABLE IF NOT EXISTS image (
+        id INT GENERATED ALWAYS AS IDENTITY,
+        info STRING,
+        format STRING,
+        resolution STRING
+    )`
+);
+
+// And then let modify it so we can add the fk key
+postGres.addModifyTable(
+    `ALTER TABLE image 
+        ADD CONSTRAINT fk_info FOREIGN KEY (info) REFERENCES media (id),
+        ADD CONSTRAINT fk_format FOREIGN KEY (format) REFERENCES imageFormat (id),
+        ADD CONSTRAINT fl_resolution FOREIGN KEY (resolution) REFERENCES resolution (id)
+    `
+);
+
+class imageDao extends IBaseDao{
+    
+    /**
+     * Search and return the image with the corresponding id.
+     * @param {number} id - The id of the image. 
+     */
+    select(id){
+
+    }
+
+
+    /**
+     * Add a image to the database and return it with it new id.
+     * @param {ImageEntity} image - The image to add.
+     */
+    commit(image){
+
+    }
+
+    /**
+     * Modify a image to the database.
+     * @param {ImageEntity} image 
+     */
+    modify(image){
+
+    }
+
+
+    /**
+     * @description Delete a image to the database.
+     * @param {ImageEntity} image 
+     */
+    delete(image){
+
+    }
+
+}
+
+module.exports = imageDao; 
