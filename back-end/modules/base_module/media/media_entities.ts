e26399674833646541341611bed7aa8e7692a092
Marc-Étienne <>
2021-12-30 22:43:03 -0500

    wip

---
+++ back-end/modules/base_module/media/media_entities.ts
@@ -0,0 +1,11 @@
+
+export interface media_groups{
+    id: number | undefined;
+    date_creation: Date | undefined;
+    type: string | undefined;
+    taille: int | undefined;
+    longueur: int | undefined;
+    thumbnail_mediaId: int | undefined;
+    nom: string | undefined;
+    mediaPath: string | undefined;
+}
